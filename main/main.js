e0bae23c6d4892bcbbf7b6df2d2cd9f7c909e3fb
Max-IThub <Nikolaev.max05@gmail.com>
2023-03-04 00:24:29 +0300

    first changes

---
+++ main/main.js
@@ -0,0 +1,22 @@
+function getNumber(max = 256, min = 0){
+    return Math.floor(Math.random() * (max-min) + min)
+}
+function getColor(a = 100){
+    return `rgba(${getNumber()},
+    ${getNumber()},${getNumber()}, ${a / 100})`
+}
+function getGradient(cnt = 2, type = "linear", dir){
+    let str = ""
+    switch(type){
+        case "linear": str += `linear-gradient(`; break;
+        case "radial": str += `radial-gradient(`; break;
+    }
+    if(dir){
+        str += dir + ","
+    }
+    while(cnt--){
+        str += getColor(getNumber(101));
+        str += cnt ? "," : ")";
+    }
+    return str
+}
